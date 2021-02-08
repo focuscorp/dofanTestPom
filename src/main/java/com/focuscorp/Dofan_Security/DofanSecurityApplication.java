59191369d0da081789518b0f3c1cb7af063ea2a0
wiem-bc <wiem.benchaabane@focus-corporation.com>
2021-02-08 21:11:39 +0100

    first structure commit

---
+++ src/main/java/com/focuscorp/Dofan_Security/DofanSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.focuscorp.Dofan_Security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DofanSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DofanSecurityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/focuscorp/Dofan_Security/DofanSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.focuscorp.Dofan_Security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DofanSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
