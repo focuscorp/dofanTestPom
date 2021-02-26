3e54c5a6fe8b0088ae20845f308087ecae994b3f
RahmaKar <rahma.karboul@focus-corporation.com>
2021-02-26 11:28:32 +0100

    Solve issues

--- src/main/java/com/focuscorp/Dofan_Security/repository/UserRepository.java
+++ src/main/java/com/focuscorp/Dofan_Security/repository/UserRepository.java
@@ -1,16 +1,21 @@
 package com.focuscorp.Dofan_Security.repository;
 
 import com.focuscorp.Dofan_Security.model.User;
-import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.repository.CrudRepository;
 
 import java.util.Optional;
 
-public interface UserRepository extends MongoRepository<User, Long>{
+public interface UserRepository extends CrudRepository<User, String>{
     //public User findByName(String name);
+
+    User findByEmail(String email);
 
     Optional<User> findByUsername(String username);
 
     Boolean existsByUsername(String username);
 
     Boolean existsByEmail(String email);
+
+    User findByEmailIgnoreCase(String email);
+    
 }
