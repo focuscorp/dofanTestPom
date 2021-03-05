cfd124e4942f3326fb36773851403089d4e86b23
wiem-bc <wiem.benchaabane@focus-corporation.com>
2021-03-05 01:02:01 +0100

    add facebook login

--- src/main/java/com/focuscorp/Dofan_Security/DofanSecurityApplication.java
+++ src/main/java/com/focuscorp/Dofan_Security/DofanSecurityApplication.java
@@ -5,7 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication(exclude = { SecurityAutoConfiguration.class, DataSourceAutoConfiguration.class })
+//@SpringBootApplication(exclude = { SecurityAutoConfiguration.class, DataSourceAutoConfiguration.class })
+@SpringBootApplication
 public class DofanSecurityApplication {
 
 	public static void main(String[] args) {
