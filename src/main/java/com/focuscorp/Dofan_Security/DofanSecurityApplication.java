abe62a31645df3d9bf847e12664b250c51799366
RahmaKar <rahma.karboul@focus-corporation.com>
2021-03-10 16:41:35 +0100

    Logging in xml file using Log4j tool

--- src/main/java/com/focuscorp/Dofan_Security/DofanSecurityApplication.java
+++ src/main/java/com/focuscorp/Dofan_Security/DofanSecurityApplication.java
@@ -5,11 +5,20 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
+import org.apache.log4j.Logger;
+
 @SpringBootApplication(exclude = { SecurityAutoConfiguration.class, DataSourceAutoConfiguration.class })
 public class DofanSecurityApplication {
-
+	public static Logger log = Logger.getLogger(DofanSecurityApplication.class);
 	public static void main(String[] args) {
-		SpringApplication.run(DofanSecurityApplication.class, args);
+		//SpringApplication.run(DofanSecurityApplication.class, args);
+
+		//--- Print Msg ---
+        log.debug("From debug");
+        log.info("From Info");
+        log.warn("From warn");
+        log.error("From error");
+        log.fatal("From fatal");
 	}
 
 }
